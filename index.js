3bd056043d87afd2b8c1e0fc23b8ae5128a7cd0f
r6hk416 <r6hk416@gmail.com>
2021-01-22 12:31:13 +0900

    add

--- index.js
+++ index.js
@@ -36,9 +36,9 @@ client.on("message", (message) => {
   
   if (message.content == "!현재시간") {
     let embed = new Discord.MessageEmbed()
+    embed.setColor("ff8e8e")
     .setTitle("현재시간")
     .setURL("https://search.naver.com/search.naver?where=nexearch&sm=top_hty&fbm=1&ie=utf8&query=%ED%98%84%EC%9E%AC%EC%8B%9C%EA%B0%84")
-    .addField("자세한 시간은 여기로","https://search.naver.com/search.naver?where=nexearch&sm=top_hty&fbm=1&ie=utf8&query=%ED%98%84%EC%9E%AC%EC%8B%9C%EA%B0%84")
     .setTimestamp()
     .setFooter("화이트해커 가 만듬")
 
